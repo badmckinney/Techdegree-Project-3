9f18793e8180b55835556d10230642a749fa0447
Brad McKinney <brad.mckin@gmail.com>
2018-06-04 22:28:04 -1000

    Added onchange event to hide or show other title input field if "other" is selected for title

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+//Hides "other-title" input field by default. Shows IF "other" option is selected from drop down menu
+const title = document.getElementById('title')
+const otherTitle = document.getElementById('other-title');
+otherTitle.style.display = "none";
 
+function showOtherTitle () {
+  if (title.value === "other") {
+    otherTitle.style.display = "block";
+  } else {
+    otherTitle.style.display = "none";
+  }
+}
