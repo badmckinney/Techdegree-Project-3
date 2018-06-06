13a178c2f01154829f1446b2c47e987f34de9413
Brad McKinney <brad.mckin@gmail.com>
2018-06-05 21:59:14 -1000

    Resolved an issue where the default color value was stuck on whatever was last selected

--- js/main.js
+++ js/main.js
@@ -21,8 +21,9 @@ const showOtherTitle = () => {
 }
 
 
-//Stores the shirt themes description in a variable to be referenced.
-//Shows color selector if a theme is chosen.
+//Creates a variable and stores the shirt themes description in that variable to be referenced.
+//Displays color selector on page if a theme is chosen.
+//Sets default color option to first available color in the list.
 const changeColorOptions = () => {
     let shirtTheme;
     if (design.value === 'Select Theme') {
@@ -30,9 +31,11 @@ const changeColorOptions = () => {
     } else if (design.value === 'js puns') {
       shirtTheme = 'JS Puns';
       shirtColorDiv.style.display = "block";
+      color.value = "cornflowerblue";
     } else if (design.value === 'heart js') {
       shirtTheme = 'I ♥ JS'
       shirtColorDiv.style.display = "block";
+      color.value = "tomato";
     }
 
 //Matches shirt theme with colors associated with that theme.
@@ -44,7 +47,4 @@ const changeColorOptions = () => {
         colorOption.style.display = "none";
       }
     });
-
-
-
 }
