7c3152517957e43d690232a1746f02c50d56b534
Brad McKinney <brad.mckin@gmail.com>
2018-06-05 19:02:53 -1000

    converted function declarations to arrow functions as per last projects review recommendations

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
-//Hides "other-title" input field by default. Shows IF "other" option is selected from drop down menu
+//Global variables and page defaults on pageload
 const title = document.getElementById('title')
 const otherTitle = document.getElementById('other-title');
+const shirtDesign = document.getElementById('design');
+const shirtColor = document.getElementById('colors-js-puns');
+const colorOptions = color.querySelectorAll('option');
+const punsColors = document.getElementsByClassName('puns');
+const heartColors = document.getElementsByClassName('heart');
 otherTitle.style.display = "none";
+shirtColor.style.display = "none";
 
-function showOtherTitle () {
+
+//Shows or hides the "other title" input field based on selected option
+const showOtherTitle = () => {
   if (title.value === "other") {
     otherTitle.style.display = "block";
   } else {
     otherTitle.style.display = "none";
+  }
+}
+
+
+//Changes the displayed color options based on the selected design option
+const changeColorOptions = () => {
+  shirtColor.style.display = "block";
+  if (shirtDesign.value === "js puns") {
+    punsColors.style.display = "none";
+    heartColors.style.display = "display";
+  } else if (shirtDesign.value === "heart js") {
+    punsColors.style.display = "none";
+    heartColors.style.display = "display";
   }
 }
